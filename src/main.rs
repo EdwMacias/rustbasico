1259ce1e4ba2ab3f962b664504f7f341cbfb7fa7
edwmaciaslinux <edwardavid26@hotmail.com>
2024-10-09 09:14:26 -0500

    docs(main): pruebas if en main
    se agrega la opcion de usar if, else if y else en una funcion que pregunta la edad del usuario para veririficar la entrada a una discoteca.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 fn main() {
-    let mut edad: u8 = 25;
-    let temperatura: i8 = -3;
-    let nombre: &str = "Edwar";
-    edad = edad + 1;
-    println!("Hola {}!", nombre);
-    println!("tengo {} anios", edad);
-    println!("hizo mucho frio, fueron {} grados", temperatura);
+    // if true{
+    //     println!("hola mundo");
+    // } else {
+    //     println!("adios mundo");
+    // }
     
     // obtener la edad de la consola
     println!("Que edad tienes?:");
-    let mut edad2: String = String::new();
-    std::io::stdin().read_line(&mut edad2).unwrap();
+    let mut edad: String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
     
     //convertir esa edad a un numero
-    let edad_int: u8 = edad2.trim().parse().unwrap();
-    println!("tienes {} anios", edad_int);
-
+    let edad_int: u8 = edad.trim().parse().unwrap();
+    if edad_int >= 18 && edad_int != 30{
+        println!("puedes entrar a la disco.");
+    }
+    else if edad_int == 30{
+        println!("Lo siento, no puedes entrar, SE PROHIBE EXCLUSIVAMENTE LA ENTRADA A PERSONAS DE 30.");
+    }
+    else{
+        println!("no puedes entrar");
+    }
+        println!("tienes {} anios", edad_int);
 }
- 
