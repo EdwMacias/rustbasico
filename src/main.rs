943fd8f320b998df138440964ce12f2146f7d5c7
edwmaciaslinux <edwardavid26@hotmail.com>
2024-10-09 11:25:15 -0500

    docs(main): pruebas loops
    uso de loops en rust con uso de if else y break

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 fn main() {
-    // if true{
-    //     println!("hola mundo");
-    // } else {
-    //     println!("adios mundo");
-    // }
-    
-    // obtener la edad de la consola
-    println!("Que edad tienes?:");
-    let mut edad: String = String::new();
-    std::io::stdin().read_line(&mut edad).unwrap();
-    
-    //convertir esa edad a un numero
-    let edad_int: u8 = edad.trim().parse().unwrap();
-    if edad_int >= 18 && edad_int != 30{
-        println!("puedes entrar a la disco.");
+    //ciclo loop de dos numeros a sumar
+    let  numero_1 = 123;
+    let numero_2 = 456;
+    let suma = numero_1 + numero_2;
+    println!("por favor, escribir la suma entre {} y {}", numero_1, numero_2);
+
+    //obtener del usuario el numero a sumar
+    loop {
+        let mut suma_usuario = String::new();
+        std::io::stdin()
+            .read_line(&mut suma_usuario)
+            .expect("Fallo al leer la entrada");
+        let suma_usuario_int: i32 = suma_usuario.trim().parse().unwrap();
+        if suma_usuario_int == suma{
+            println!("muy bien, el resultado es: {} y es correcto", suma);
+            break;
+        } else {
+            println!("el resultado es: {} y es incorrecto", suma_usuario_int);
+        }
     }
-    else if edad_int == 30{
-        println!("Lo siento, no puedes entrar, SE PROHIBE EXCLUSIVAMENTE LA ENTRADA A PERSONAS DE 30.");
-    }
-    else{
-        println!("no puedes entrar");
-    }
-        println!("tienes {} anios", edad_int);
+
 }
