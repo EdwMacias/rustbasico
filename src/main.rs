e34cf761083bb336aa613362e9a9009a8003c993
edwmaciaslinux <edwardavid26@hotmail.com>
2024-10-09 21:06:22 -0500

    docs(main): installation crates io

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
 fn main() {
-    //ciclo loop de dos numeros a sumar
-    let  numero_1 = 123;
-    let numero_2 = 456;
-    let suma = numero_1 + numero_2;
-    println!("por favor, escribir la suma entre {} y {}", numero_1, numero_2);
-
-    //obtener del usuario el numero a sumar
-    loop {
-        let mut suma_usuario = String::new();
-        std::io::stdin()
-            .read_line(&mut suma_usuario)
-            .expect("Fallo al leer la entrada");
-        let suma_usuario_int: i32 = suma_usuario.trim().parse().unwrap();
-        if suma_usuario_int == suma{
-            println!("muy bien, el resultado es: {} y es correcto", suma);
-            break;
-        } else {
-            println!("el resultado es: {} y es incorrecto", suma_usuario_int);
-        }
-    }
 
 }
