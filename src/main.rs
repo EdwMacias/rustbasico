0b9a8ef496852a04c5bc35b470525854773a110c
edwmaciaslinux <edwardavid26@hotmail.com>
2024-10-16 10:06:40 -0500

    prprogreso curso

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::arch::x86_64::_SIDD_LEAST_SIGNIFICANT;
 
+use regex::Regex;
+fn main() {
+// Regex
+let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+
+// Traer Datos del usuario
+println!("Introduce tu expresion:");
+let mut expression = String::new();
+std::io::stdin().read_line(&mut expression).unwrap();
+// Aplicar operaciones
+let caps = re_add.captures(expression.as_str()).unwrap();
+let left_value : i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+println!("{:?}", caps);
+//Mostrar resultados
+println!("Resultado: {}", expression);
 }
